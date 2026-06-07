2250993b64f37bd236a22b47a943c1c9ef80e5ef
suraj-bhardwaj-stack <surajbhardwaj000007@gmail.com>
2026-06-07 20:58:47 +0530

    Initial commit and write a first function while lreaning about functions

---
+++ warm-up/sumTwoIntergers.js
@@ -0,0 +1,28 @@
+// Sum of Two Integers
+// Problem Statement:
+// Write a Program that defines a function to calculate the sum of two integers and prints the result. Call this function by passing two integer values.
+
+// Approach:
+// Define a function that takes two numbers as input.
+// Add the two numbers inside the function.
+// Call the function with two integers & print the result.
+// Example:
+// Input: 5, 3
+
+// Process: a + b => 5 + 3 = 8
+
+// Output: 8
+
+function sum(a , b){
+    let sumNum = a + b;
+    console.log("Ans:"+ sumNum)
+}
+sum(7 , 13)
+
+// Return value 
+function findSquare(num){
+    return num * num
+}
+
+let collectSquareValue = findSquare(4)
+console.log("Square Value: " + collectSquareValue)
