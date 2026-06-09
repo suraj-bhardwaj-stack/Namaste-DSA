013d12e174ce3c6c8ea11ee4c5b7e3cc31491520
suraj-bhardwaj-stack <surajbhardwaj000007@gmail.com>
2026-06-09 15:41:53 +0530

    Start loop

---
+++ warm-up/loop.js
@@ -0,0 +1,33 @@
+for(i=0; i< 5; i++){
+    console.log(i);
+}
+
+// output 0 to 9
+
+// First check the condition 
+// Second , if condition is true , code will be exicuted 
+// if code is exicuted , 
+// print => 0
+// increment the value 0 => 0 + 1
+// check the condition again i < 5
+// if condition is true , code will be exicuted
+// if code is exicuted
+// print => 1
+// again increment the value 1 => 1 + 1
+// check the condition again i < 5
+// if condition is true , code will be exicuted
+// if code is exicuted
+// print 2
+// again increment the value 2 => 2 + 1
+// check the condition again i < 5
+// if condition is true , code will be exicuted
+// if code is exicuted
+// print 3
+// again increment the value 3 => 3 + 1
+// check the condition again i < 5
+// if condition is true , code will be exicuted
+// if code is exicuted
+// print 4
+// again increment the value 4 => 4 + 1
+// check the condition , now condition is false
+// so code exicution end
